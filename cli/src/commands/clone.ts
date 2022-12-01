cd6f5445b64ed2d61a3386249e6430c64a89f0ef
Elizabeth Murray <elizabeth@3box.io>
2022-12-01 12:50:06 -0500

    remove unused degit log & enable local config

--- cli/src/commands/clone.ts
+++ cli/src/commands/clone.ts
@@ -10,11 +10,9 @@ export default class Clone extends Command {
   static flags = Command.flags
 
   async run(): Promise<void> {
-    // TODO: Figure out a better way to do this.
     const templates = {
       "DID-DataStore":
         "https://github.com/ceramicstudio/create-ceramic-app/templates/basic-profile",
-      // TODO: update this link
       "[DEVELOPER PREVIEW] ComposeDB":
         "https://github.com/ceramicstudio/create-ceramic-app/templates/composedb-profile",
     };
@@ -49,7 +47,6 @@ export default class Clone extends Command {
       this.spinner.start("cloning...");
       // FUTURE: Allow users to provide their own URL to clone from. If no full url is provided (including a .com/ca/xyz etc) assume it's one of
         // our defaults & use the appropriate URL.
-      console.log(answers.template)
       const emitter = degit(
         templates[answers.template],
         {
@@ -59,7 +56,6 @@ export default class Clone extends Command {
       );
       
       emitter.on('info', info => {
-        console.log(info)
         if(info.code === 'SUCCESS') {
           this.spinner.succeed(`Project cloned successfully at ${chalk.green.bold(info.dest)}`);
         }
